503f88e66d48be0d8f7ef92c3bc804dd7b48dde2
elusivemansiv <mansibahsan9@gmail.com>
2025-09-24 11:40:27 +0600

    updated

--- ExampleProject/Framework/Pages/AlertPage.cs
+++ ExampleProject/Framework/Pages/AlertPage.cs
@@ -8,9 +8,19 @@ namespace ExampleProject.Framework.Pages
     {
         private IButton alertCard = ElementFactory.GetButton(By.XPath("//h5[text()='Alerts, Frame & Windows']"), "Alerts Card");
         private IButton alertsOption = ElementFactory.GetButton(By.XPath("//span[text()='Alerts']"), "Alerts");
-        private IButton clickMe => ElementFactory.GetButton(By.Id("alertButton"), "Click Button to see alert");
-        private ILabel successmsglbl => ElementFactory.GetLabel(By.XPath(string.Format(LocatorConstants.PreciseTextLocator,
-            "You clicked a button")), "Success message text");
+        //private IButton clickMe => ElementFactory.GetButton(By.Id("alertButton"), "Click Button to see alert");
+        //private IButton clickMe2 => ElementFactory.GetButton(By.Id("confirmButton"), "On button click, confirm box will appear");
+
+
+        //private ILabel successmsglbl => ElementFactory.GetLabel(By.XPath(string.Format(LocatorConstants.PreciseTextLocator,
+        //    "You clicked a button")), "Success message text");
+
+        //private ILabel successmsglbl2 => ElementFactory.GetLabel(By.XPath(string.Format(LocatorConstants.PreciseTextLocator,
+        //    "Do You confirm action")), "Success message text");
+
+        private IButton promptButton = ElementFactory.GetButton(By.Id("promtButton"), "Prompt alert button");
+
+        private ILabel promptResult = ElementFactory.GetLabel(By.Id("promptResult"), "Prompt result text");
 
 
         public AlertPage() : base(By.ClassName("category-cards"), "Alert Page")
@@ -28,7 +38,19 @@ namespace ExampleProject.Framework.Pages
             alertsOption.Click();
         }
 
-        public void ClickClickMeBtn() => clickMe.Click();
-        public bool IsSuccessmsgDisplayed() => successmsglbl.State.IsDisplayed;
+        public void ClickClickMeBtn()
+        {
+            //clickMe.Click();
+            //clickMe2.Click();
+            promptButton.Click();
+        }
+        //public bool IsSuccessmsgDisplayed() => successmsglbl.State.IsDisplayed;
+
+        //public bool IsSuccessmsgDisplayed2() => successmsglbl2.State.IsDisplayed;
+
+     
+        public bool IsPromptResultDisplayed() => promptResult.State.IsDisplayed;
+
+        public string GetPromptResultText() => promptResult.GetText();
     }
 }
--- ExampleProject/Framework/Tests/AlertTest.cs
+++ ExampleProject/Framework/Tests/AlertTest.cs
@@ -1,6 +1,10 @@
-﻿using ExampleProject.Framework;
+﻿using Aquality.Selenium.Browsers;
+using Aquality.Selenium.Core.Utilities;
+using Aquality.Selenium.Elements.Actions;
+using ExampleProject.Framework;
 using ExampleProject.Framework.Pages;
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace ExampleProject.Framework.Tests
 {
@@ -15,9 +19,23 @@ namespace ExampleProject.Framework.Tests
             alertPage.ClickAlertsOption();
             
             alertPage.ClickClickMeBtn();
-            browser.HandleAlert(Aquality.Selenium.Browsers.AlertAction.Accept);
-            alertPage.IsSuccessmsgDisplayed();
-            
+            //browser.HandleAlert(Aquality.Selenium.Browsers.AlertAction.Accept);
+            //browser.HandleAlert(Aquality.Selenium.Browsers.AlertAction.Decline);
+            //alertPage.IsSuccessmsgDisplayed();
+            //alertPage.IsSuccessmsgDisplayed2();
+
+            var driver = AqualityServices.Browser.Driver;
+            IAlert promptAlert = driver.SwitchTo().Alert();
+
+            string name = "Mansib";
+            promptAlert.SendKeys(name);
+            promptAlert.Accept();
+
+            // Verify result
+            Assert.IsTrue(alertPage.IsPromptResultDisplayed(), "Prompt result message is not displayed.");
+            StringAssert.Contains(name, alertPage.GetPromptResultText(), "The entered name is not displayed in the result.");
+
+
         }
     }
 }
